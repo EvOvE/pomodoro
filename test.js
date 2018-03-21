5acbbc8b6884b17217e999991e567dfbff1d11ea
Super Developer <email@dev.com>
2018-03-21 20:52:22 +0300

    Hello world

---
+++ test.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+// create a server object
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World!');
+    res.end();
+}).listen(8080)
